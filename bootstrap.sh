9d27dd1601cc49f2c26822bedf4367388bb18df2
Uttej Guduru <uguduru.18@gmail.com>
2020-07-12 14:56:00 -0400

    Add some MAS apps

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,30 @@
+#!/bin/bash
+
 # Ask for admin password upfront
 sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Intall developer tools if not already installed
+xcode-select -p
+if [ $? -eq 0 ]; then
+    echo "Found XCode."
+else
+    echo "Installing XCode..."
+    xcode-select --install
+fi
+
+# Since XCode was installed, accept license agreement
+sudo xcodebuild -license
+
+# Allow third party software
+sudo spctl --master-disable
+
+# Flags
+set -e          # Global exit on error flag
+set -o pipefail # Exit on pipe error
+set -x          # Higher verbosity for easier debug
 
 which -s brew
 if [[ $? != 0 ]] ; then
@@ -19,6 +44,10 @@ brew upgrade
 
 # Install Zsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+echo "Installing fonts for Zsh..."
+git clone https://github.com/powerline/fonts.git
+./fonts/install.sh
+rm -rf fonts
 
 # Install Cask
 echo "Checking brew cask..."
@@ -26,11 +55,16 @@ brew install caskroom/cask/brew-cask &>/dev/null
 brew upgrade brew-cask &>/dev/null
 
 which -s java || brew cask install java
-which -s git || brew install git
+which -s jenv || brew cask install jenv
+
 which -s sbt || brew install sbt
 which -s scala || brew install scala
 which -s ammonite-repl || brew install ammonite-repl
+
 which -s node || brew install node
+which -s nvm || brew install nvm
+
+which -s git || brew install git
 which -s mysql || brew install mysql
 which -s wget || brew install wget
 which -s ack || brew install ack
@@ -39,6 +73,8 @@ which -s llvm || brew install llvm
 # Generic softwares
 brew cask install 1password
 brew cask install adobe-acrobat-reader
+brew cask install alfred
+brew cask install anki
 brew cask install avast-security
 brew cask install bettertouchtool
 brew cask install caffeine
@@ -59,6 +95,18 @@ brew cask install spotify
 brew cask install vlc
 brew cask install whatsapp
 brew cask install zoomus
+
+# Mac App Store Installs
+brew install mas
+mas "Annotate", id: 918207447
+mas "Caffeine", id: 411246225                       # 
+mas "feedly", id: 865500966 
+mas "Monosnap", id: 540348655
+mas "PhotoScape X", id: 929507092
+mas "Spark", id: 1176895641                         # Good e-Mail client
+mas "TweetDeck", id: 485812721
+mas "Twitter", id:409789998
+mas "Who's On My WiFi", id: 909760813               # Easily search devices on wifi and store MAC addresses
 
 
 # Security Tools
