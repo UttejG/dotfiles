606802fa8595b7961a2e4e7e41faa69f57b327ec
Uttej Guduru <uguduru.18@gmail.com>
2021-07-06 16:52:13 -0700

    Use Brewfile

---
+++ Brewfile
@@ -0,0 +1,98 @@
+tap "coursier/formulas"
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/cask-drivers"
+tap "homebrew/cask-versions"
+tap "homebrew/core"
+tap "homebrew/services"
+tap "ruimarinho/tap"
+brew "ammonite-repl"
+brew "awscli"
+brew "bdw-gc"
+brew "cmake"
+brew "exiftool"
+brew "jenv"
+brew "jq"
+brew "leiningen"
+brew "mas"
+brew "mysql", restart_service: true
+brew "pyenv"
+brew "python@3.8"
+brew "re2"
+brew "sbt"
+brew "scala"
+brew "sphinx-doc"
+brew "tldr"
+brew "coursier/formulas/coursier"
+brew "ruimarinho/tap/gsts"
+cask "adobe-acrobat-reader"
+cask "alfred"
+cask "alt-tab"
+cask "amazon-photos"
+cask "anki"
+cask "brave-browser"
+cask "coconutbattery"
+cask "discord"
+cask "docker"
+cask "dropbox"
+cask "evernote"
+cask "firefox"
+cask "flux"
+cask "garmin-express"
+cask "github"
+cask "gitter"
+cask "google-chrome"
+cask "google-earth-pro"
+cask "google-nik-collection"
+cask "imageoptim"
+cask "intellij-idea"
+cask "iterm2"
+cask "java"
+cask "java8"
+cask "lightworks"
+cask "mysql-utilities"
+cask "mysqlworkbench"
+cask "omnidisksweeper"
+cask "postman"
+cask "pycharm"
+cask "quik"
+cask "screenhero"
+cask "scroll-reverser"
+cask "sequel-pro"
+cask "skype"
+cask "slack"
+cask "spotify"
+cask "tableplus"
+cask "thinkorswim"
+cask "tor-browser"
+cask "typora"
+cask "virtualbox"
+cask "visual-studio-code"
+cask "vlc"
+cask "whatsapp"
+mas "AdBlock", id: 1402042596
+mas "Adblock Plus", id: 1432731683
+mas "Annotate", id: 918207447
+mas "Caffeine", id: 411246225
+mas "Dropover", id: 1355679052
+mas "Evernote Web Clipper", id: 1481669779
+mas "feedly", id: 865500966
+mas "GarageBand", id: 682658836
+mas "Honey", id: 1472777122
+mas "iMovie", id: 408981434
+mas "Keynote", id: 409183694
+mas "Kindle", id: 405399194
+mas "Microsoft To Do", id: 1274495053
+mas "Monosnap", id: 540348655
+mas "Notability", id: 360593530
+mas "Numbers", id: 409203825
+mas "Pages", id: 409201541
+mas "PhotoScape X", id: 929507092
+mas "Slack", id: 803453959
+mas "Solar App", id: 450545814
+mas "Spark", id: 1176895641
+mas "Trello", id: 1278508951
+mas "TweetDeck", id: 485812721
+mas "Twitter", id: 1482454543
+mas "Wunderlist", id: 410628904
+mas "Xcode", id: 497799835
--- bootstrap.sh
+++ bootstrap.sh
@@ -35,11 +35,9 @@ fi
 # Update brew
 echo "Updating brew..."
 brew update &>/dev/null
-brew tap homebrew/dupes &>/dev/null
-brew tap homebrew/boneyard &>/dev/null
-brew install homebrew/dupes/grep &>/dev/null
 
-#Upgrade already installed formulae
+# Upgrade already installed formulae
+echo "Upgrade already existing formulae..."
 brew upgrade
 
 # Install Zsh
@@ -49,83 +47,8 @@ git clone https://github.com/powerline/fonts.git
 ./fonts/install.sh
 rm -rf fonts
 
-# Install
-echo "Checking brew..."
-brew installroom/cask/brew-cask &>/dev/null
-brew upgrade brew-cask &>/dev/null
-
-which -s java || brew install java
-which -s jenv || brew install jenv
-
-which -s sbt || brew install sbt
-which -s scala || brew install scala
-which -s ammonite-repl || brew install ammonite-repl
-
-which -s node || brew install node
-which -s nvm || brew install nvm
-
-which -s git || brew install git
-which -s mysql || brew install mysql
-which -s wget || brew install wget
-which -s ack || brew install ack
-which -s llvm || brew install llvm
-
-# Generic software
-brew install 1password
-brew install adobe-acrobat-reader
-brew install alfred
-brew install anki
-brew install avast-security
-brew install bettertouchtool
-brew install caffeine
-brew install coconutbattery
-brew install dropbox
-brew install evernote
-brew install firefox
-brew install flashlighttool
-brew install garmin-express
-brew install google-chrome
-brew install kindle
-brew install microsoft-office
-brew install omnidisksweeper
-brew install quik
-brew install scroll-reverser
-brew install skype
-brew install spotify
-brew install vlc
-brew install whatsapp
-brew install zoomus
-
-# Mac App Store Installs
-brew install mas
-mas "Annotate", id: 918207447
-mas "Caffeine", id: 411246225                       # 
-mas "feedly", id: 865500966 
-mas "Monosnap", id: 540348655
-mas "PhotoScape X", id: 929507092
-mas "Spark", id: 1176895641                         # Good e-Mail client
-mas "TweetDeck", id: 485812721
-mas "Twitter", id:409789998
-mas "Who's On My WiFi", id: 909760813               # Easily search devices on wifi and store MAC addresses
-
-
-# Security Tools
-brew install keybase
-brew install torbrowser
-
-# Developer tools
-brew install diffmerge
-brew install docker
-brew install gitter
-brew install github
-brew install iterm2
-brew install intellij-idea
-brew install postman
-brew install sequel-pro
-brew install slack
-brew install sourcetree
-brew install visual-studio-code
-brew install virtualbox
+# Install using Brewfile
+brew bundle
 
 # Cleanup
 brew cleanup
